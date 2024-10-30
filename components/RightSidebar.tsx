2a542b4d65693e43961f992b3394677deb20ab04
SurajOne8 <ysuraj18333@gmail.com>
2024-10-30 22:28:23 +0530

    Fixed right side bar issue

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -36,7 +36,7 @@ const RightSidebar = ({
     return (
         <section className='"flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-2-[227px] sticky right-0 h-full max-sm:hidden select-none'>
             <h3 className='px-5 pt-4 text-xs uppercase'>design</h3>
-            <span className='text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4'>Srj's Canvas for Your Use</span>
+            <span className='text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4'>Srj&apos;s Canvas for Your Use</span>
 
             <Dimensions
                 width={elementAttributes.width}
